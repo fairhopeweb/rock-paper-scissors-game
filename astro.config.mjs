ce39b00239f2a2b40078ab370d13350e84c220c8
Mark Teekman <mark.teekman@gmail.com>
2022-06-01 16:30:45 +0200

    Add site configuration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://markteekman.github.io',
+  base: '/rock-paper-scissors-game',
   vite: {
     ssr: {
       external: ['svgo']
